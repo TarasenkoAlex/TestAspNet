12c165a4aa21904fcb19f48a10f7e7814cfc0470
Alex Tarasenko <tarasenk93@gmail.com>
2019-04-22 18:18:22 +0300

    Первые шаги в ASP.Net Core MVC.

--- TestAspNet/Controllers/HomeController.cs
+++ TestAspNet/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TestAspNet.Models;
 
@@ -10,34 +7,34 @@ namespace TestAspNet.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Godd Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
         }
 
-        public IActionResult About()
+        [HttpPost]
+        public ViewResult RsvpForm(QuestResponse questResponse)
         {
-            ViewData["Message"] = "Your application description page.";
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(questResponse);
+                return View("Thanks", questResponse);
+            }
 
             return View();
         }
 
-        public IActionResult Contact()
+        public ViewResult ListResponses()
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(Repository.Responses.Where(el => el.WillAttend == true));
         }
     }
 }
---
+++ TestAspNet/Models/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TestAspNet.Models
+{
+    public static class Repository
+    {
+        private static List<QuestResponse> _responses = new List<QuestResponse>();
+
+        public static IEnumerable<QuestResponse> Responses => _responses;
+
+        public static void AddResponse(QuestResponse response) => _responses.Add(response);
+    }
+}
